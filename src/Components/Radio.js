fabfaf9165178df8257d729c2ce999f2cf74a003
Nlparra <53584490+Nlparra@users.noreply.github.com>
2020-07-09 20:57:44 -0400

    worked on fetch code

--- src/Components/Radio.js
+++ src/Components/Radio.js
@@ -1,11 +1,29 @@
-import React from "react"
+import React, { useCallback } from "react"
+
 
 
 
  export default function Radio (){
+    // const [music , setMusic] = useState()
+
+    
+          let getMusic = () => {
+            fetch('http://api.shoutcast.com/legacy/genresearch?k=${.env.APIKEY}&genre=classic')
+            .then (res=>res.json())
+            .then (data => {
+                console.log(data);
+                // this.setState({tweet:data.message})
+
+                
+            })
+            .catch(err=>{console.log(err);
+            })
+        }
+
  return (
     <>
     <div>
+        <button onClick={getMusic}></button>
         <audio controls>
             <source src="horse.ogg" type="audio/ogg" />
         <source src="horse.mp3" type="audio/mpeg" />
